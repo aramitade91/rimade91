e7d45bb0de7d4f311c5ad72ac50b848fcef0e513
ARAMITA DE <aramitade91@gmail.com>
2023-07-11 23:25:54 +0530

    Did some changes.

--- Aramita.py
+++ Aramita.py
@@ -1,1 +1,15 @@
-print(2)
+import numpy as np
+
+# Input the array elements
+arr = np.array([1, 2, 3, 4, 5])
+
+# Print the array
+print(arr)
+import pandas as pd
+
+# Load the CSV file into a DataFrame
+file_path = r'C:\Users\ARAMITA DE\Desktop\New folder (3)\world-data-2023.csv'
+df = pd.read_csv(file_path)
+
+# Print the DataFrame
+print(df)
